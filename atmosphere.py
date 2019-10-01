bf28fb447ccdfc24af950d7f00adb73df3341f58
Douglas Bergman <bergmandr@gmail.com>
2019-10-01 10:46:06 -0500

    Changed to conform with python naming standards
    The class name is now Atmosphere (and now no confusions with the method atmosphere).
    All s (for self) references in instance methods are written out as self.
    All 'not type(h) is np.ndarray' changed to 'type(h) is not np.ndarray'

--- atmosphere.py
+++ atmosphere.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import quad
 
-class atmosphere:
+class Atmosphere:
     """
     Class containing constants and methods for using the US Standard Atmosphere of 1976
     
@@ -21,7 +21,7 @@ class atmosphere:
     gas_constant          = 8.31432   # J/MolK
     gMR           = gravity * air_mol_weight / gas_constant
 
-    def __init__(s,altitudes=None,rel_pressure=None,temperatures=None,temp_gradient=None):
+    def __init__(self,altitudes=None,rel_pressure=None,temperatures=None,temp_gradient=None):
         """
         Create and instance of an atmospheric model. 
 
@@ -41,22 +41,22 @@ class atmosphere:
         """
         if altitudes is None and rel_pressure is None and \
            temperatures is None and temp_gradient is None:
-            s.altitudes    = np.array([0.,11000.,20000.,32000.,47000.,51000.,71000.,84852.])            # m above SL
-            s.rel_pressure = np.array([1.,
+            self.altitudes    = np.array([0.,11000.,20000.,32000.,47000.,51000.,71000.,84852.])            # m above SL
+            self.rel_pressure = np.array([1.,
                                        2.23361105092158e-1,5.40329501078488e-2,8.56667835929167e-3,
                                        1.09456013377711e-3,6.60635313285837e-4,3.90468337334393e-5,
                                        3.68500952357479e-6])
-            s.temperatures = np.array([288.15,216.65,216.65,228.65,270.65,270.65,214.65,186.946]) # K
-            s.temp_gradient = np.array([-0.0065,0.,0.001,0.0028,0.,-0.0028,-0.002,0.])    # K/m
+            self.temperatures = np.array([288.15,216.65,216.65,228.65,270.65,270.65,214.65,186.946]) # K
+            self.temp_gradient = np.array([-0.0065,0.,0.001,0.0028,0.,-0.0028,-0.002,0.])    # K/m
         else:
-            s.altitudes     = altitudes
-            s.rel_pressure  = rel_pressure
-            s.temperatures  = temperatures
-            s.temp_gradient = temp_gradient
-        s.maximum_height = s.altitudes[-1]
-        s.minimum_height = s.altitudes[0]
+            self.altitudes     = altitudes
+            self.rel_pressure  = rel_pressure
+            self.temperatures  = temperatures
+            self.temp_gradient = temp_gradient
+        self.maximum_height = self.altitudes[-1]
+        self.minimum_height = self.altitudes[0]
 
-    def atmosphere(s,h):
+    def atmosphere(self,h):
         """
         This function returns atmospheric temperature, pressure, and density as a function of height.
         
@@ -68,39 +68,39 @@ class atmosphere:
             P   - pressure [Pa]
             rho - density [kg/m3]
         """
-        if not type(h) is np.ndarray:
+        if type(h) is not np.ndarray:
             h = np.array([h],dtype=float)
             nin = 0
         else:
             nin = len(h)
 
         # Find the entry in the tables for each height
-        too_low  = h < s.minimum_height
-        too_high = h > s.maximum_height
-        indx  = np.searchsorted(s.altitudes,h,side='right')
+        too_low  = h < self.minimum_height
+        too_high = h > self.maximum_height
+        indx  = np.searchsorted(self.altitudes,h,side='right')
         idx = indx - 1
 
         # Find the temperature at height
-        altitude        = s.altitudes[idx]
-        base_temp       = s.temperatures[idx]
-        temp_gradient   = s.temp_gradient[idx]
+        altitude        = self.altitudes[idx]
+        base_temp       = self.temperatures[idx]
+        temp_gradient   = self.temp_gradient[idx]
         delta_altitude  = h - altitude
         temperature     = base_temp + temp_gradient*delta_altitude
 
         # Find the relative pressure at height
-        base_rel_pressure = s.rel_pressure[idx]
+        base_rel_pressure = self.rel_pressure[idx]
         flat = np.abs(temp_gradient) < 1.e-10
         rel_pressure = np.empty_like(h)
         rel_pressure[flat]  = base_rel_pressure[flat]  * \
-                              np.exp(-s.gMR/1000*delta_altitude[flat]/base_temp[flat])
+                              np.exp(-self.gMR/1000*delta_altitude[flat]/base_temp[flat])
         rel_pressure[~flat] = base_rel_pressure[~flat] * \
-                              (base_temp[~flat]/temperature[~flat])**(s.gMR/1000/temp_gradient[~flat])
-        pressure = rel_pressure * s.pressure_sea_level
-        density  = rel_pressure * s.density_sea_level * s.temperature_sea_level/temperature 
+                              (base_temp[~flat]/temperature[~flat])**(self.gMR/1000/temp_gradient[~flat])
+        pressure = rel_pressure * self.pressure_sea_level
+        density  = rel_pressure * self.density_sea_level * self.temperature_sea_level/temperature 
 
-        temperature[too_low] = s.temperature_sea_level
-        pressure[too_low]    = s.pressure_sea_level
-        density[too_low]     = s.density_sea_level
+        temperature[too_low] = self.temperature_sea_level
+        pressure[too_low]    = self.pressure_sea_level
+        density[too_low]     = self.density_sea_level
         temperature[too_high] = 0.
         pressure[too_high]    = 0.
         density[too_high]     = 0.
@@ -114,7 +114,7 @@ class atmosphere:
         else:
             return T,P,rho
 
-    def temperature(s,h):
+    def temperature(self,h):
         """
         This function returns temperature as a function of height.
         
@@ -124,10 +124,10 @@ class atmosphere:
         Returns:
             T - temperature [K]
         """
-        T,_,_ = s.atmosphere(h)
+        T,_,_ = self.atmosphere(h)
         return(T)
 
-    def pressure(s,h):
+    def pressure(self,h):
         """
         This function returns pressure as a function of height.
         
@@ -137,10 +137,10 @@ class atmosphere:
         Returns:
             P - pressure [Pa]
         """
-        _,P,_ = s.atmosphere(h)
+        _,P,_ = self.atmosphere(h)
         return(P)
     
-    def density(s,h):
+    def density(self,h):
         """
         This function returns density as a function of height.
         
@@ -150,10 +150,10 @@ class atmosphere:
         Returns:
             rho - density [kg/m3]
         """
-        _,_,rho = s.atmosphere(h)
+        _,_,rho = self.atmosphere(h)
         return(rho)
     
-    def delta(s,h):
+    def delta(self,h):
         """
         This function returns the difference of the index-of-refraction from unity.
         
@@ -163,11 +163,11 @@ class atmosphere:
         Returns:
             delta - equal to n - 1.
         """
-        T,P,_ = s.atmosphere(h)
+        T,P,_ = self.atmosphere(h)
         P /= 1000.       # Pa -> kPa
         return 7.86e-4*P/T
         
-    def depth(s,h1,h2=None):
+    def depth(self,h1,h2=None):
         """
         This function returns atmospheric depth. It is the integral of atmospheric density between two heights.
         
@@ -187,16 +187,16 @@ class atmosphere:
         
         """
         if h2 is None:
-            h2 = s.maximum_height*np.ones_like(h1)
+            h2 = self.maximum_height*np.ones_like(h1)
             
-        if not type(h1) is np.ndarray and not type(h2) is np.ndarray:
+        if type(h1) is not np.ndarray and type(h2) is not np.ndarray:
             h1 = np.array([h1],dtype=float)
             h2 = np.array([h2],dtype=float)
             nin = 0
-        elif not type(h2) is np.ndarray:
+        elif type(h2) is not np.ndarray:
             h2 = h2*np.ones_like(h1)
             nin = len(h1)
-        elif not type(h1) is np.ndarray:
+        elif type(h1) is not np.ndarray:
             h1 = h1*np.ones_like(h2)
             nin = len(h2)
         else:
@@ -204,12 +204,12 @@ class atmosphere:
 
         A = h1.copy()
         B = h2.copy()
-        A[A<s.minimum_height] = s.minimum_height
-        B[B<s.minimum_height] = s.minimum_height
-        A[A>s.maximum_height] = s.maximum_height
-        B[B>s.maximum_height] = s.maximum_height
+        A[A<self.minimum_height] = self.minimum_height
+        B[B<self.minimum_height] = self.minimum_height
+        A[A>self.maximum_height] = self.maximum_height
+        B[B>self.maximum_height] = self.maximum_height
 
-        depth = np.array([quad(s.density,a,b)[0] for a,b in zip(A,B)])
+        depth = np.array([quad(self.density,a,b)[0] for a,b in zip(A,B)])
         depth /= 10. # 1 km/m2 == 1000/10,000 g/cm2
 
         if nin == 0:
@@ -217,7 +217,7 @@ class atmosphere:
         else:
             return depth
 
-    def slant_depth(s,theta,d1,d2=None):
+    def slant_depth(self,theta,d1,d2=None):
         """
         This function returns atmospheric depth as a function of the slant angle with respect to the vertical.
         
@@ -239,37 +239,37 @@ class atmosphere:
             The slant depth from d2 to d1 at angle theta. [g/cm2]
         """
         if d2 is None:
-            d2 = s.maximum_height/np.cos(theta)
+            d2 = self.maximum_height/np.cos(theta)
 
-        if not type(theta) is np.ndarray and \
-           not type(d1) is np.ndarray and \
-           not type(d2) is np.ndarray:
+        if type(theta) is not np.ndarray and \
+           type(d1) is not np.ndarray and \
+           type(d2) is not np.ndarray:
             theta = np.array([theta],dtype=float)
             d1 = np.array([d1],dtype=float)
             d2 = np.array([d2],dtype=float)
             nin = 0
-        elif not type(d1) is np.ndarray and \
-             not type(d2) is np.ndarray:
+        elif type(d1) is not np.ndarray and \
+             type(d2) is not np.ndarray:
             d1 = d1*np.ones_like(theta)
             d2 = d2*np.ones_like(theta)
             nin = len(theta)
-        elif not type(theta) is np.ndarray and \
-             not type(d2) is np.ndarray:
+        elif type(theta) is not np.ndarray and \
+             type(d2) is not np.ndarray:
             theta = theta*np.ones_like(d1)
             d2 = d2*np.ones_like(d1)
             nin = len(d1)
-        elif not type(theta) is np.ndarray and \
-             not type(d1) is np.ndarray:
+        elif type(theta) is not np.ndarray and \
+             type(d1) is not np.ndarray:
             theta = theta*np.ones_like(d2)
             d1 = d1*np.ones_like(d2)
             nin = len(d2)
-        elif not type(theta) is np.ndarray:
+        elif type(theta) is not np.ndarray:
             theta = theta*np.ones_like(d1)
             nin = min(len(d1),len(d2))
-        elif not type(d1) is np.ndarray:
+        elif type(d1) is not np.ndarray:
             d1 = d1*np.ones_like(theta)
             nin = min(len(theta),len(d2))
-        elif not type(d2) is np.ndarray:
+        elif type(d2) is not np.ndarray:
             d2 = d2*np.ones_like(theta)
             nin = min(len(theta),len(d1))
         else:
@@ -278,25 +278,25 @@ class atmosphere:
         costheta = np.cos(theta)
         A = d1.copy()
         B = d2.copy()
-        A[A<s.minimum_height] = s.minimum_height
-        B[B<s.minimum_height] = s.minimum_height
-        bigA = A>s.maximum_height/costheta
-        A[bigA] = s.maximum_height/costheta[bigA]
-        bigB = B>s.maximum_height/costheta
-        B[bigB] = s.maximum_height/costheta[bigB]
+        A[A<self.minimum_height] = self.minimum_height
+        B[B<self.minimum_height] = self.minimum_height
+        bigA = A>self.maximum_height/costheta
+        A[bigA] = self.maximum_height/costheta[bigA]
+        bigB = B>self.maximum_height/costheta
+        B[bigB] = self.maximum_height/costheta[bigB]
 
         h1 = A*costheta
         h2 = B*costheta
 
         if nin == 0:
-            return s.depth(h1,h2)/costheta[0]
+            return self.depth(h1,h2)/costheta[0]
         else:
-            return s.depth(h1,h2)/costheta
+            return self.depth(h1,h2)/costheta
         
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     plt.ion()
-    us77 = atmosphere()
+    us77 = Atmosphere()
     h = np.linspace(0,100000,101)
     T,P,rho = us77.atmosphere(h)
     X = us77.depth(h)
